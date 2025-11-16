a7c2b94a2772afa3fdc3dfb53b95483a1e9e2794
Dominik Seitz <ds243@hdm-stuttgart.de>
2025-11-16 13:15:53 +0100

    add: dependency and startup code

--- app/main.py
+++ app/main.py
@@ -1,19 +1,35 @@
+from typing import Union
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.models import Cryptocurrency
 from app.db import SessionLocal
-from app.models import CryptoPrice
 from scripts.init_db import init_db
 
-
-def list_cryptos():
+def list_cryptocurrencies():
     db = SessionLocal()
-    cryptos = db.query(CryptoPrice).all()
-    for c in cryptos:
-        print(f"{c.symbol}: {c.price}")
+    crypto_currencies = db.query(Cryptocurrency).all()
+    for c in crypto_currencies:
+       print(f"{c.fullName}: ({c.symbol})")
     db.close()
-    return cryptos  # Add this line to return the cryptos list
+    return crypto_currencies
 
-
-if __name__ == "__main__":
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Connecting to DB...")
     init_db()
     print("=== Crypto Prices ===")
-    cryptos = list_cryptos()
+    cryptos = list_cryptocurrencies()
     print(f"Loaded {len(cryptos)} cryptos from DB.")
+
+    yield
+
+    # Clean up the ML models and release the resources
+    print("Closing DB connection...")
+app = FastAPI(lifespan=lifespan)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
